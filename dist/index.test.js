fb92dd458ae990eee7d684f7729bbfe1b3a9a3ae
David Torralba Goitia <david.torralba.goitia@gmail.com>
2018-03-08 08:43:49 +0000

    Start implementing TogglApi promised based wrappers

---
+++ dist/index.test.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+require("mocha");
+describe('Friday', () => {
+    it('20180202', () => '20180202');
+});
+//# sourceMappingURL=index.test.js.map
