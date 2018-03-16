ab72519e172e0e8f72ed59321b7e4f6db35393eb
David Torralba Goitia <david.torralba.goitia@gmail.com>
2018-03-16 07:05:49 +0000

    Implement code coverage with Istambul

--- dist/index.test.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-require("mocha");
-describe('Friday', () => {
-    it('20180202', () => '20180202');
-});
-//# sourceMappingURL=index.test.js.map
